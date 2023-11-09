0406b76af591934a598083b80196e7c064deae9b
spencergrigg <spencegrigg@gmail.com>
2023-11-09 15:04:31 -0700

    Completed LearningActivity2 & worked on TeamDesign

---
+++ prepare/Learning02/Job.cs
@@ -0,0 +1,15 @@
+public class Job
+
+{
+    public string _jobTitle;
+    public string _company;
+
+    public double _startYear;
+
+    public double _endYear;
+
+    public void Display()
+    {
+        Console.WriteLine($"{_jobTitle} ({_company}) {_startYear}-{_endYear}");
+    }
+}
