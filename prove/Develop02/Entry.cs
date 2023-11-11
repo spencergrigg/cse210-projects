cb03d51e0734163582961f64327a48fb37be6840
Spencer Grigg <132022430+spencergrigg@users.noreply.github.com>
2023-11-11 15:26:06 -0700

    Update Entry.cs
    Commented out unnecessary code

--- prove/Develop02/Entry.cs
+++ prove/Develop02/Entry.cs
@@ -3,17 +3,17 @@ using System.ComponentModel.DataAnnotations;
 
 public class Entry
 {
-    public DateTime theCurrentTime = DateTime.Now;
+    //public DateTime theCurrentTime = DateTime.Now;
     public string _date;
     public string _promptText;
-    public string _entryText = Console.ReadLine();
-    public Entry()
-    {
-        _date = theCurrentTime.ToShortDateString();
-    }
+    public string _entryText;
+    //public Entry()
+    //{
+    //    _date = theCurrentTime.ToShortDateString();
+    //}
 
-    PromptGenerator prompt = new PromptGenerator();
-    _promptText = prompt.GetRandomPrompt();
+    //PromptGenerator prompt = new PromptGenerator();
+    //_promptText = prompt.GetRandomPrompt();
     
 
     public void Display()
