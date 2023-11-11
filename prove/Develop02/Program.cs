c6bf6d1545a6d019b0440a7c3398072eee717e56
Spencer Grigg <132022430+spencergrigg@users.noreply.github.com>
2023-11-11 15:13:58 -0700

    Update Program.cs
    Included comments to help with writing the program

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -6,6 +6,13 @@ class Program
     {
         Journal theJournal = new Journal();
         Entry anEntry = new Entry();
+        
         anEntry.Display();
     }
 }
+
+//Write a new entry - Show the user a random prompt, save the respons, the prompt, and the date as an Entry
+//Display the journal - Iterate through all entries in the journal and display to the screen
+//Save the journal to a file - Prompt the user for a filename and save the current journal (all entries) to that file location
+//Load the journal from a file - Get filename & load the journal (all entries from the file). This should replace any entries currently stored in the journal
+//Provide a menu that allows the user to choose these options
