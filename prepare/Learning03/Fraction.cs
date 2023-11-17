946f8c48e19882521b1b7abaad95c6f7979a4481
spencergrigg <spencegrigg@gmail.com>
2023-11-16 17:34:52 -0700

    Completed W3 Learning Activity

---
+++ prepare/Learning03/Fraction.cs
@@ -0,0 +1,55 @@
+public class Fraction
+{
+private int _top;
+private int _bottom;
+
+ public Fraction()
+ {
+    _top = 1;
+    _bottom = 1;
+ }
+
+public Fraction(int wholeNumber)
+{
+    _top = wholeNumber;
+    _bottom = 1;
+}
+
+public Fraction(int top, int bottom)
+{
+    _top = top;
+    _bottom = bottom;
+}
+
+public int GetTop()
+{
+    return _top;
+}
+
+public void SetTop(int top)
+{
+    _top = top;
+}
+
+public int GetBottom()
+{
+    return _bottom;
+}
+
+public void SetBottom(int bottom)
+{
+    _bottom = bottom;
+}
+
+public string GetFractionString()
+{
+    string _fractionString = $"{_top}/{_bottom}";
+    return _fractionString;
+    
+}
+public double GetDecimalValue()
+{
+    double _decimal = (_top / 1.0 ) /_bottom;
+    return _decimal;
+}
+}
