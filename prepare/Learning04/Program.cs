907344487dacc08d4f8f185e06c3d52073975eb7
spencergrigg <spencegrigg@gmail.com>
2023-11-23 10:11:00 -0700

    Finished week4 prepare

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        //Assignment ass1 = new Assignment();
+        //Console.WriteLine(ass1.GetSummary());
+
+        Assignment ass2 = new Assignment("Bob Jones", "Math");
+        Console.WriteLine(ass2.GetSummary());    
+
+        MathAssignment mass1 = new MathAssignment("Frank Joe", "Calculus 1", "Section 1", "Problems 1-10");
+        Console.Write(mass1.GetSummary());
+        Console.WriteLine(mass1.GetHomeworkList());
+
+        WritingAssignment wass1 = new WritingAssignment("Elly Mae", "ELA 4", "Write an Essay");
+        Console.Write(wass1.GetSummary());
+        Console.WriteLine(wass1.GetWritingInformation());
     }
 }
