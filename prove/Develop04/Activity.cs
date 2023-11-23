26fde1fa0f132363d18b734d2efc0b50d23f9fd2
spencergrigg <spencegrigg@gmail.com>
2023-11-22 22:18:28 -0700

    Created classes with stubs for Week4 Team Activity

---
+++ prove/Develop04/Activity.cs
@@ -0,0 +1,16 @@
+public class Activity{
+    private string _name;
+    private string _description;
+    private int _duration;
+    
+    //public void DisplayStartingMessage();
+    //private void DisplayEndingMessage();
+    //private void ShowSpinner(int seconds);
+    //ShowCountDown(int second);
+    
+    public Activity(string name, string description, int duration){
+        _name = name;
+        _description = description;
+        _duration = duration;
+    }
+}
---
+++ prove/Develop04/ReflectionActivity.cs
@@ -0,0 +1,18 @@
+public class ReflectionActivity 
+{
+    private List<string> _prompts;
+    private List<string> _questions;
+
+//     public ReflectingActivity() : base(name, description, duration)
+// {
+//     _name = "Reflecting";
+//     _description = "This activity will help you reflect on times ...";
+//     _duration = 50;
+// }
+
+    //public void Run();
+    //public string GetPrompt();
+    //public string GetRandomQuestion();
+    //public void DisplayPrompt();
+    //public void DisplayQuestions();
+}
